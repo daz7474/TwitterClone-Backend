2ff3902ee9e76ee7f8de59327c71a3d4ee0a60b7
daz7474 <daz7474@gmail.com>
2024-05-30 15:07:13 +0100

    Added endpoint for user data

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,10 +27,15 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'username' => fake()->unique()->userName(),
             'avatar' => 'https://i.pravatar.cc/150?img=' . fake()->numberBetween(1, 70),
+            'profile' => fake()->sentence(14),
+            'location' => fake()->city() . ',' . fake()->country(),
+            'link' => 'https://laravasts.com',
+            'linkText' => collect(['laracasts.com', 'laravel.com'])->random(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'created_at' => fake()->dateTimeBetween('-5 years', now()),
         ];
     }
 
