a834193457ccf934781f7d1dc72c926c3f7990af
daz7474 <daz7474@gmail.com>
2024-05-30 11:18:54 +0100

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Models\Tweet;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/tweets', function () {
+    return Tweet::with('user:id,name,username,avatar')->latest()->get();
+});
